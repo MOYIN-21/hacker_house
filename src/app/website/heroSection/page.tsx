9faf41d1dc39bbdd380ed8c5172b54e75be0a5e2
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-04 14:42:09 +0100

    Hero section

---
+++ src/app/website/heroSection/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {Component} from 'react';
+import HeroSection from "@/component/Hero/HeroSection";
+
+class Page extends Component {
+    render() {
+        return (
+            <HeroSection/>
+        );
+    }
+}
+
+export default Page;
--- src/component/Hero/HeroSection.tsx
+++ src/component/Hero/HeroSection.tsx
@@ -1,20 +1,58 @@
 "use client"
 import React, {Component} from 'react';
 import Image from "next/image";
+import {CircleArrowRight} from "lucide-react";
 
 
-
-class Student extends Component {
+class HeroSection extends Component {
     render() {
         return (
-            <div className="grid grid-rows-[20px_1fr_20px] bg-blue-950  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <Image
-                    aria-hidden
-                    src="/assets/image/semicolon-logo.svg"
-                    alt="Globe icon"
-                    width={100}
-                    height={100}
-                />
+            <div
+                className="w-full grid grid-cols  bg-blue-950 rounded-bl-[100px] md:h-screen items-center justify-items-center">
+                <div className={`flex flex-row space-x-4 pt-4`}>
+                    <div>
+                        <Image
+                            aria-hidden
+                            src="/assets/image/semicolon-logo.svg"
+                            alt="semicolon icon"
+                            width={200}
+                            height={600}
+                        />
+                    </div>
+                    <div className="h-7 border-l-2 border-white-500"></div>
+                    <div>
+                        <Image
+                            aria-hidden
+                            src="/assets/image/sui-logo.svg"
+                            alt="Globe icon"
+                            width={60}
+                            height={50}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <Image
+                        aria-hidden
+                        src="/assets/image/HackerHouseNew.svg"
+                        alt="HackerHouseNew icon"
+                        width={500}
+                        height={500}
+                    />
+                </div>
+                <div className={`flex md:flex-row flex-col md:space-x-5 space-y-4 items-center justify-items-center`}>
+                    <div>
+                        <p className={`text-3xl text-cyan-400 italic font-sans`}>Road to Sui Overflow</p>
+                    </div>
+                    <div className={`flex flex-row`}>
+                        <button
+                            type="button"
+                            className="flex gap-1 text-blue-950 bg-cyan-400 hover:bg-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+                        >
+                            Apply
+                            <CircleArrowRight className="w-4 h-4 pt-1 text-blue-950"/>
+                        </button>
+                    </div>
+                </div>
             </div>
 
 
@@ -22,4 +60,4 @@ class Student extends Component {
     }
 }
 
-export default Student;
+export default HeroSection;

9a509fe02efd5c02827813ea1dfbcc7ef3520028
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-04 18:10:58 +0100

    lucide react icons dependency was removed while react icons dependency was added

---
+++ src/app/website/about/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {Component} from 'react';
+import About from "@/component/about/About";
+
+class Page extends Component {
+    render() {
+        return (
+            <About/>
+        );
+    }
+}
+
+export default Page;

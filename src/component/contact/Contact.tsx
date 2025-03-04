1c86a1c1ec018e32c9bf407ec28f727f83524e72
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-04 16:15:07 +0100

    Contact component

---
+++ src/component/contact/Contact.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import {Calendar, MapPin, Clock} from "lucide-react";
+import InfoCard from "@/reusables/InfoCard";
+
+const Contact = () => {
+    return (
+        <div className={`flex items-center justify-items-center`}>
+            <div className="flex items-center justify-items-center max-w-3xl p-6 bg-white border rounded-lg shadow-sm ">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                    <InfoCard icon={<Calendar className="h-10 w-10"/>} title="March 24-29," description="2025"/>
+                    <InfoCard
+                        icon={<MapPin className="h-10 w-10"/>}
+                        title="312 Herbert Macaulay,"
+                        description="Sabo, Yaba, Lagos, Nigeria"
+                    />
+                    <InfoCard
+                        icon={<Clock className="h-10 w-10"/>}
+                        title="Application Deadline:"
+                        description="March 12, 2025"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;

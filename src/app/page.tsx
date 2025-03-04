d3e3a7be8e32192ad6a5b8ac666e9793d7feacf4
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-04 16:17:42 +0100

    refactored was done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import HeroSection from "@/component/Hero/HeroSection";
+import HeroSection from "@/component/hero/HeroSection";
+import Contact from "@/component/contact/Contact";
 
 export default function Home() {
   return (
-    <main className="w-full bg-white items-center justify-items-center min-h-screen pb-20 gap-16">
-        <HeroSection/>
-    </main>
+      <main className="relative bg-white flex flex-col items-center min-h-screen pb-20 gap-16">
+          <HeroSection />
+          <div className="w-full flex justify-center -mt-24 sm:-mt-24 md:-mt-28 lg:-mt-24">
+              <Contact />
+          </div>
+      </main>
   );
 }

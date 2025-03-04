065f58b3b4b367564110ff57b28309b6ae1cf43c
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-04 16:16:31 +0100

    contact page

---
+++ src/app/website/contact/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {Component} from 'react';
+import Contact from "@/component/contact/Contact";
+
+class Page extends Component {
+    render() {
+        return (
+            <Contact/>
+        );
+    }
+}
+
+export default Page;

b1a1bab8dde708ba93b5493dbe97490f2d51dc2c
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-05 08:37:23 +0100

    ParticipantRequirement page

---
+++ src/app/website/ParticipantRequirement/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {Component} from 'react';
+import ParticipantRequirement from "@/component/participant requirement/Participant_requirement";
+
+class Page extends Component {
+    render() {
+        return (
+            <ParticipantRequirement/>
+        );
+    }
+}
+
+export default Page;

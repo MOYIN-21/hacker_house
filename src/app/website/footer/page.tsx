29d599002f80f1e5c0620e1f4fd631392518a492
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-05 08:12:13 +0100

    footer page

---
+++ src/app/website/footer/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {Component} from 'react';
+import Footer from "@/component/footer/Footer";
+
+class Page extends Component {
+    render() {
+        return (
+            <Footer/>
+        );
+    }
+}
+
+export default Page;

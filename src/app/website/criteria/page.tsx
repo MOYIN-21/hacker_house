ab0d0fabd3791b9424f8abe7a3053d2fdedb698d
MO YIN OLUWA <olawamidemoyinoluwamary@gmail.com>
2025-03-05 07:30:58 +0100

    criteria page

---
+++ src/app/website/criteria/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, {Component} from 'react';
+import Criteria from "@/component/criteria/Criteria";
+
+class Page extends Component {
+    render() {
+        return (
+            <Criteria/>
+        );
+    }
+}
+
+export default Page;
